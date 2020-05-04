e9d2da0d0cbd992e684958d279d9f02a787d325c
Lucia <l.cyprianova@gmail.com>
2020-05-04 19:23:21 +0200

    photo and description in js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,9 @@
-$('.currentPhoto').css('background-image', 'url("./fotky/jesienka.jpg")')
+let data = {
+    photo: 'fotky/jesienka.jpg',
+    title: 'Rozkvitnutá jesienka',
+    description: 'Hrebene Nízkych Tatier sa farbia dofialova, keď si suchou trávou na svetlo sveta svoju cestu prerážajú jesienky.'
+};
+
+$('#photo').attr('src', data.photo);
+$('#photo-title').html(`<h1>${data.title}</h1>`);
+$('#photo-description').html(`<p>${data.description}</p>`);
