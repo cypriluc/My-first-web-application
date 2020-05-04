c995aba0c948b487bc63f0eee6f3326960328270
Lucia <l.cyprianova@gmail.com>
2020-05-04 21:43:32 +0200

    js: data and imagesData
    for each picture create and object "data" - containing link to the picture, title and description
    make a list of data -> "imagesData

--- scripts.js
+++ scripts.js
@@ -1,9 +1,42 @@
-let data = {
+let data_0 = {
     photo: 'fotky/jesienka.jpg',
     title: 'Rozkvitnutá jesienka',
     description: 'Hrebene Nízkych Tatier sa farbia dofialova, keď si suchou trávou na svetlo sveta svoju cestu prerážajú jesienky.'
 };
 
-$('#photo').attr('src', data.photo);
-$('#photo-title').html(`<h1>${data.title}</h1>`);
-$('#photo-description').html(`<p>${data.description}</p>`);
+let data_1 = {
+    photo: 'fotky/jesterka.jpg',
+    title: 'Jašterička',
+    description: 'Jašterička vystrkuje jazýček.'
+};
+
+let data_2 = {
+    photo: 'fotky/krizna.jpg',
+    title: 'Hrebeň Krížnej',
+    description: 'Posledné zbytky jarného snehu na Krížnej.'
+};
+
+let data_3 = {
+    photo: 'fotky/modry_kvietok.jpg',
+    title: 'Modrý kvietok',
+    description: 'Modrý kvietok zvláštneho druhu rastie v bezfarebnom okolí.'
+};
+
+let data_4 = {
+    photo: 'fotky/prasiva.jpg',
+    title: 'Hrebeň Krížnej',
+    description: 'Posledné zbytky jarného snehu na Prašivej.'
+};
+
+let data_5 = {
+    photo: 'fotky/snezienka.jpg',
+    title: 'Snežienka',
+    description: 'Prvá jarná, len čo zasvieti slniečko a miznú zbytky snehu, už kvitne.'
+};
+
+let currentPhoto = 0;
+let imagesData = [data_0, data_1, data_2, data_3, data_4, data_5]
+
+$('#photo').attr('src', imagesData[currentPhoto].photo);
+$('#photo-title').html(`<h1>${imagesData[currentPhoto].title}</h1>`);
+$('#photo-description').html(`<p>${imagesData[currentPhoto].description}</p>`);
